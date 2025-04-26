a3237a80030c98cd1435eb624f5d2555bb49d5d9
Chris Ware <c.p.ware@hotmail.com>
2025-04-25 20:33:48 -0700

    Meet us done

--- src/components/homePage/MeetUs/MeetUs.tsx
+++ src/components/homePage/MeetUs/MeetUs.tsx
@@ -23,7 +23,14 @@ export default function MeetUs() {
               </div>
               <div className={styles.tcRight}>
                 <p className={styles.copy}>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil, inventore sunt. Ipsam sint nihil enim delectus natus, sunt explicabo ab vero rem, labore excepturi, commodi architecto autem nostrum odit sequi ad rerum itaque repudiandae blanditiis. Praesentium non impedit numquam dolor inventore minima porro sit, quisquam reiciendis, a eius molestias cum illum nisi delectus rerum nemo expedita consectetur maxime? Mollitia velit doloribus rerum ratione nam facere, possimus minima voluptatibus molestias consequatur, ipsa blanditiis pariatur quisquam quibusdam sequi, corrupti eaque. Consectetur dolores facilis, sit illo accusantium nulla nesciunt neque ullam, laudantium quibusdam dignissimos recusandae eveniet iste quod nostrum cum nisi voluptas magni!
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Nihil, inventore sunt. Ipsam sint nihil enim delectus natus,
+                  sunt explicabo ab vero rem, labore excepturi, commodi
+                  architecto autem nostrum odit sequi ad rerum itaque
+                  repudiandae blanditiis. Praesentium non impedit numquam dolor
+                  inventore minima porro sit, quisquam reiciendis, a eius
+                  molestias cum illum nisi delectus rerum nemo expedita
+                  consectetur maxime?
                 </p>
                 <div className={styles.btnContainer}>
                   <Button btnType='primary' href='/' text='Learn More' />
@@ -31,7 +38,24 @@ export default function MeetUs() {
               </div>
             </div>
           </div>
-          <div className={styles.bottom}></div>
+          <div className={styles.bottom}>
+            <div className={styles.bTop}>
+              <video
+                preload='auto'
+                autoPlay
+                muted
+                loop
+                className={styles.videoii}
+              >
+                <source src='./videos/video1.mp4' />
+              </video>
+            </div>
+            {/* <div className={styles.bBottom}>
+              <h4 className={styles.subHeading}>Audo House</h4>
+              <p className={styles.copy}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet adipisci inventore consequatur voluptate iure sunt vero sapiente illo? Totam libero sequi pariatur deserunt aspernatur voluptatibus quidem rerum repellendus cumque magni?</p>
+              <p className={styles.copy}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet adipisci inventore consequatur voluptate iure sunt vero sapiente illo? Totam libero sequi pariatur deserunt aspernatur voluptatibus quidem rerum repellendus cumque magni?</p>
+            </div> */}
+          </div>
         </div>
       </LayoutWrapper>
     </section>
