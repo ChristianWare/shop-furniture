5c5d4d3bd5cd03d0748f5879918f99d2aea0e1fa
Chris Ware <c.p.ware@hotmail.com>
2025-04-26 22:09:19 -0700

    Meet us fix

--- src/components/homePage/MeetUs/MeetUs.tsx
+++ src/components/homePage/MeetUs/MeetUs.tsx
@@ -27,10 +27,7 @@ export default function MeetUs() {
                   Nihil, inventore sunt. Ipsam sint nihil enim delectus natus,
                   sunt explicabo ab vero rem, labore excepturi, commodi
                   architecto autem nostrum odit sequi ad rerum itaque
-                  repudiandae blanditiis. Praesentium non impedit numquam dolor
-                  inventore minima porro sit, quisquam reiciendis, a eius
-                  molestias cum illum nisi delectus rerum nemo expedita
-                  consectetur maxime?
+                  repudiandae blanditiis.
                 </p>
                 <div className={styles.btnContainer}>
                   <Button btnType='primary' href='/' text='Learn More' />
@@ -50,11 +47,6 @@ export default function MeetUs() {
                 <source src='./videos/video1.mp4' />
               </video>
             </div>
-            {/* <div className={styles.bBottom}>
-              <h4 className={styles.subHeading}>Audo House</h4>
-              <p className={styles.copy}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet adipisci inventore consequatur voluptate iure sunt vero sapiente illo? Totam libero sequi pariatur deserunt aspernatur voluptatibus quidem rerum repellendus cumque magni?</p>
-              <p className={styles.copy}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet adipisci inventore consequatur voluptate iure sunt vero sapiente illo? Totam libero sequi pariatur deserunt aspernatur voluptatibus quidem rerum repellendus cumque magni?</p>
-            </div> */}
           </div>
         </div>
       </LayoutWrapper>
